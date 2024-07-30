c36834270812c07c1807232c7d6e912d37b8cd9d
Modin Sergei <jetleebruce@gmail.com>
2024-07-30 08:55:50 +0300

    Add Minifier for css and setup css modules for style isolation

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
-import './Counter.scss';
+import styles from './Counter.module.scss';
 
 export const Counter = () => {
     const [count, setCount] = useState(0);
+    // console.log(classes.btn);
     return (
-        <div>
+        <div className={styles.btn}>
             <div>{count}</div>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={() => setCount(count + 1)} >Increment</button>
         </div>
     );
 }
